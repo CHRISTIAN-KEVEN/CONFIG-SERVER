49257fd10a9a7937b8a6faca21817ba2fd179259
AJEITOH CHRISTIAN KEVEN <christiankeven326@gmail.com>
2024-05-22 07:20:32 +0100

    Configured config server

---
+++ src/test/java/com/example/configservicer/ConfigServicerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configservicer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServicerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
